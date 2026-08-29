d54267d4025ea164144948dc850449888b55b69f
Anjali kumari <anjalikumari67688@gmail.com>
2026-08-29 19:30:32 +0530

    Added conditionals.py

---
+++ conditionals.py
@@ -0,0 +1,14 @@
+a = int(input("enter your age:"))
+
+# if elif else ladder
+if(a>=18):
+    print("you are elligible to vote")
+
+elif(a<0):
+    print("invalid age")
+elif(a==0):
+    print("invalid age")    
+
+else:
+    print("not elligible to vote")    
+print("thank you for using this program")    
