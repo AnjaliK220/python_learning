b9abc11f0c7d1437669843928c90be8255bcfe6e
Anjali kumari <anjalikumari67688@gmail.com>
2026-08-30 21:48:25 +0530

    Initial commit

---
+++ functions.py
@@ -0,0 +1,22 @@
+# functioan defenition
+def avg():
+    a = int(input("enter number 1:"))
+    b = int(input("enter number 1:"))
+    c = int(input("enter number 1:"))
+    average = (a + b + c)/3
+    print(average)
+
+# function call
+avg()
+print("hello")
+avg()
+avg()
+
+# for 5 times result , print avg() 5 times
+
+
+
+
+
+
+    
